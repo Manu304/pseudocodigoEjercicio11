d96defe8ba7bc5334b60693aafe1d6697a3604d4
Manuel Rojas <manuerjs70@gmail.com>
2021-06-10 02:28:37 -0600

    Arregle algoritmo del ejercicio11

--- Ejercicio11Parte2.java
+++ Ejercicio11Parte2.java
@@ -11,16 +11,38 @@ import java.util.Scanner;
 //		n�meros, ahora se leen n�meros hasta que llega el Valor 36?
 public class Ejercicio11Parte2 {
     public static void main(String[] args) {
-        int paresC = 0, imparesC = 0, paresT = 0, docena = 0, numeroM = 0;
+        int paresC = 0, imparesC = 0, paresT = 0, docena = 0, numeroM = 0, num = 0;
+        float paresP;
         for(int i = 0; i < 10; i++){
-            do {
-                int num;
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("Ingrese un numero entre el 0 y el 36: ");
                 num = scanner.nextInt();
-            } while ((num >= 36) && (num <= 0));
+                if (num != 0) {
+                    if ((num%2) == 0) {
+                        paresC++;
+                        paresT += num;
+                    }else{
+                        imparesC++;
+                    }
+
+                    if ((num >= 13) && (num <= 24)) {
+                        docena++;
+                    }
+
+                    if ((num >= numeroM)) {
+                        numeroM = num;
+                    }
+                }
         }
-        
+        if (paresC == 0) {
+            paresP = (float) 0;
+        }else{
+            paresP = (float) paresT / paresC;
+        }
+        System.out.println("\nLa cantidad de numeros impares es: " + imparesC + "." 
+        + "\nEl promedio de los numeros pares es: " + paresP + "." 
+        + "\nLa cantidad de numeros que se encuentran en la 2da docena son: " + docena + "." 
+        + "\nEl numero mas grande es: " + numeroM + "."
+        + "\nSi en lugar de leer 10 numeros lee hasta que llegue al valor 36 + el ciclo PARA se repetira muchas mas veces.\n");
     }
-    
 }
